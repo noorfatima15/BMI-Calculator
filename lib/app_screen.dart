058250a705b1a52fb604b5eb247994147ad04adb
noorfatima15 <noorfatima13689@gmail.com>
2023-03-26 18:44:39 +0500

    -Project Structure has been updated and necessary dependencies has been added in pubspec.yaml

--- lib/app_screen.dart
+++ lib/app_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'presentation/screens/splash_screen.dart';
+
 class AppScreen extends StatelessWidget {
   const AppScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: SplashScreen(),
+    );
   }
 }
---
+++ lib/presentation/screens/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:bmi_calculator/presentation/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 10), () {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+          (route) => false);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+      ),
+    );
+  }
+}
