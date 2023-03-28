7fe8c8a94201e0a660e79084859f0c59f8f39f2a
noorfatima15 <noorfatima13689@gmail.com>
2023-03-28 23:59:24 +0500

    -Color Constant has been Created.

---
+++ lib/constants/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  ColorPalette._();
+
+  ///Main Colors
+  static const Color primaryColor = Color(0xFF0A0E21);
+  static const Color activeCardColor = Color(0xFF1D1E33);
+  static const Color inactiveCardColor = Color(0xFF111328);
+  static const Color secondaryColor = Color(0xFFEB1555);
+  static const Color secondarySplashColor = Color(0x26C41C4C);
+
+  ///Text Colors
+  static const Color primaryHeader = Color(0xFFFFFFFF);
+  static const Color labelColor = Color(0xFF8D8E98);
+}
--- lib/core/widgets/primary_button.dart
+++ lib/core/widgets/primary_button.dart
@@ -5,11 +5,13 @@ class PrimaryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: () {},
-      child: const Text('Calculate'),
-      minWidth: MediaQuery.of(context).size.width,
-      color: const Color(0xFFEB1555),
+    return Align(
+      child: MaterialButton(
+        onPressed: () {},
+        minWidth: MediaQuery.of(context).size.width,
+        color: const Color(0xFFEB1555),
+        child: const Text('Calculate'),
+      ),
     );
   }
 }
